6edb4c30b417794d9789b140b38a3a4125b36914
Javed Habeeb <javedhabeeb.ixc@gmail.com>
2025-12-17 11:22:07 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-sudo install -m 755 suvib /usr/local/bin/suvib
+sudo install -m 755 stream.sh /usr/local/bin/suvib
 
 echo "Installed suvib to /usr/local/bin"
 echo "User data will be stored in ~/.local/share/suvib"
