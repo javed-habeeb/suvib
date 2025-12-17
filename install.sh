0de7fcbcc65fbcbaf9c0afd73b048c21222ce08e
Javed Habeeb <javedhabeeb.ixc@gmail.com>
2025-12-17 11:18:00 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,7 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 set -e
 
-echo "[*] setting up the environment...."
+sudo install -m 755 suvib /usr/local/bin/suvib
 
-touch links.txt link_titles.txt
-echo "[*] created links.txt and link_titles.txt (if they didnt exist)"
-
-curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o yt-dlp
-chmod +x yt-dlp
-sudo mv yt-dlp /usr/local/bin/
-echo "[*]yt-dlp installed.."
-
-#uncomment the line depending on your system
-#sudo apt update && sudo apt install -y dos2unix
-#sudo pacman -S --noconfirm dos2unix
-#brew install dos2unix
-
-echo "[*] remember to install dos2unix manually if you skip the lines above"
-
-echo "[*] symlink suvib to /usr/local/bin/"
-sudo ln -s "$(pwd)/suvib" /usr/local/bin/suvib 
-echo "[*] done"
-
-echo "[*] installation complete"
+echo "Installed suvib to /usr/local/bin"
+echo "User data will be stored in ~/.local/share/suvib"
